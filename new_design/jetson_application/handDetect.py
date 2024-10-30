8299669f778253329c9a828774107508dae3c317
thammgh37243 <hoanganh03052003@gmail.com>
2024-10-30 12:07:57 +0700

    HA: test stream v2

--- new_design/jetson_application/handDetect.py
+++ new_design/jetson_application/handDetect.py
@@ -5,13 +5,7 @@ import camera_var
 from ultralytics import YOLO
 import threading
 
-pipeline = (
-    "v4l2src device=/dev/video0 ! "
-    "video/x-raw, width=640, height=480, framerate=30/1 ! "
-    "videoconvert ! "
-    "queue max-size-buffers=1 leaky=downstream ! "
-    "appsink"
-)
+
 
 class HandDetectHandler(threading.Thread):
     def __init__(self):
@@ -35,6 +29,13 @@ class HandDetectHandler(threading.Thread):
 
     def cam_proc(self):
         """Camera processing to detect hand with position stabilization"""
+        pipeline = (
+            "v4l2src device=/dev/video0 ! "
+            "video/x-raw, width=640, height=480, framerate=30/1 ! "
+            "videoconvert ! "
+            "queue max-size-buffers=1 leaky=downstream ! "
+            "appsink"
+        )
         cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
         object_positions = []
         accumulate_count = 0
@@ -47,43 +48,43 @@ class HandDetectHandler(threading.Thread):
                 break
 
             # Undistort the frame using calibration values
-            frame = cv2.undistort(frame, camera_var.K_array, camera_var.Dis_array, None, camera_var.New_array)
-            results = self.model(frame, verbose=False)
+            # frame = cv2.undistort(frame, camera_var.K_array, camera_var.Dis_array, None, camera_var.New_array)
+            # results = self.model(frame, verbose=False)
 
-            current_positions = []
-            for result in results:
-                for box in result.boxes.xyxy:
-                    x_min, y_min, x_max, y_max = box
-                    x_center = (x_min + x_max) / 2
-                    y_center = (y_min + y_max) / 2
-                    current_positions.append((x_center, y_center))
-                    cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (255, 0, 0), 2)
-                    cv2.circle(frame, (int(x_center), int(y_center)), 5, (0, 255, 0), -1)
+            # current_positions = []
+            # for result in results:
+            #     for box in result.boxes.xyxy:
+            #         x_min, y_min, x_max, y_max = box
+            #         x_center = (x_min + x_max) / 2
+            #         y_center = (y_min + y_max) / 2
+            #         current_positions.append((x_center, y_center))
+            #         cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (255, 0, 0), 2)
+            #         cv2.circle(frame, (int(x_center), int(y_center)), 5, (0, 255, 0), -1)
 
             # Stabilize the detected position by accumulating over multiple frames
-            if len(current_positions) == 1:
-                if len(object_positions) == 0:
-                    object_positions = current_positions
-                    accumulate_count = 1
-                else:
-                    stable = True
-                    for old_pos, new_pos in zip(object_positions, current_positions):
-                        if abs(old_pos[0] - new_pos[0]) > 5 or abs(old_pos[1] - new_pos[1]) > 5:
-                            stable = False
-                            break
-                    if stable:
-                        accumulate_count += 1
-                    else:
-                        object_positions = current_positions
-                        accumulate_count = 1
+            # if len(current_positions) == 1:
+            #     if len(object_positions) == 0:
+            #         object_positions = current_positions
+            #         accumulate_count = 1
+            #     else:
+            #         stable = True
+            #         for old_pos, new_pos in zip(object_positions, current_positions):
+            #             if abs(old_pos[0] - new_pos[0]) > 5 or abs(old_pos[1] - new_pos[1]) > 5:
+            #                 stable = False
+            #                 break
+            #         if stable:
+            #             accumulate_count += 1
+            #         else:
+            #             object_positions = current_positions
+            #             accumulate_count = 1
 
-                # If the hand position has been stable for 3 consecutive frames, return the position
-                if accumulate_count >= 3:
-                    cap.release()
-                    # cv2.destroyAllWindows()
-                    accumulate_count = 0
-                    time.sleep(0.3)
-                    return object_positions  # Return stabilized hand position
+            #     # If the hand position has been stable for 3 consecutive frames, return the position
+            #     if accumulate_count >= 3:
+            #         cap.release()
+            #         # cv2.destroyAllWindows()
+            #         accumulate_count = 0
+            #         time.sleep(0.3)
+            #         return object_positions  # Return stabilized hand position
 
             # Display the frame with bounding boxes
             cv2.imshow("YOLOv8 Real-Time", frame)
