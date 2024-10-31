467cedf278343beacdbbd9cc8db325f16559e9c7
nguyen.an913 <52287427+4d4mworth1844@users.noreply.github.com>
2024-10-31 19:21:57 +0700

    Jetson: init code for gripper - further modify

---
+++ new_design/6dof_robot_control/Gripper.cpp
@@ -0,0 +1,2 @@
+#include "Gripper.h"
+
---
+++ new_design/6dof_robot_control/Gripper.h
@@ -0,0 +1,25 @@
+#ifndef __GRIPPER__H
+#define __GRIPPER__H
+
+#include "Arduino.h"
+#include "global.h"
+#include "Communication.h"
+
+/*************************************************************
+ ** Class Gripper
+ * Implementing controlling driver for gripper using for robot
+ * arm application.
+ *************************************************************/
+class Gripper {
+
+/* Private attributes and methods */
+private:
+    int gripperPin; 
+    float currentAngle; 
+/* Public methods*/
+public:
+
+
+
+}; 
+#endif
